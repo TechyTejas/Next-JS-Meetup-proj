56ea798891b0686f88454cbc2ab151942b90ab5e
tejas <sadadetej.27@gmail.com>
2023-08-02 09:31:59 +0530

    Fetching and showing data dynamically

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -1,50 +1,59 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
- 
+import { MongoClient, ObjectId } from "mongodb";
 
-function MeetupDetails () {
+function MeetupDetails (props) {
     return (
         <MeetupDetail
-        title="First MeetUp"
-        image="https://content.skyscnr.com/m/19c1f54952cdf28a/original/GettyImages-532519763.jpg?resize=800px:600px&quality=100"
-        address="Agra"
-        description="Taj Mahal"/>
+        title={props.meetupData.title}
+        image={props.meetupData.image}
+        address={props.meetupData.address}
+        description={props.meetupData.description}/>
     )
 }
 
 export async function getStaticPaths ( ) {
+    const client= await MongoClient.connect('mongodb+srv://tejassadade645:tejas2001@cluster0.5ypnjt7.mongodb.net/meetups?retryWrites=true&w=majority')
+    const db=client.db()
+
+     const meetupsCollection=db.collection('meetups')
+     const meetups=await meetupsCollection.find({}, {_id: 1}).toArray();  // this mthod will only include the id but no other values
+
+    //  client.close();
+    
     return{
         fallback:false,
-        paths:[
-            {
-                params:{
-                    meetupid:'m1'
-                },
-            },
-            {
-                params:{
-                    meetupid:'m2'
-                }
-            }    
-        ]
-    }
+        paths : meetups.map((meetup) => ({ 
+            params : { meetupid: meetup._id.toString() },
+    })),
+    };
 }
 
-export  async function getStaticProps (context) {
+export async function getStaticProps(context) {
+    // Fetch the data for a single meetup
+    const meetupid = context.params.meetupid; // Fix the typo here, change meetupid to meetupId
+
+    const client= await MongoClient.connect('mongodb+srv://tejassadade645:tejas2001@cluster0.5ypnjt7.mongodb.net/meetups?retryWrites=true&w=majority')
+    const db=client.db()
+
+     const meetupsCollection=db.collection('meetups')  // this is fr accessing the collection
     
-    const meeetupid=context.params.meetupid;
-    
+     const selectMeetup = await meetupsCollection.findOne({_id: new ObjectId(meetupid)});  // findOne finds one single dcc
+     //ObjectId will covnert string into object id object
+     
+     client.close();
+  
     return {
-        props:{
-        mmetupData:{
-            id:'meetupid',
-            title:"First MeetUp",
-        image:"https://content.skyscnr.com/m/19c1f54952cdf28a/original/GettyImages-532519763.jpg?resize=800px:600px&quality=100",
-        address:"Agra",
-        description:"Taj Mahal"
+      props: {
+        meetupData: {
+            id : selectMeetup._id.toString(),
+            title : selectMeetup.title,
+            address : selectMeetup.address,
+            image : selectMeetup.image,
+            description : selectMeetup.description
         }
-    }
-}
-
-}
+      },
+    };
+  }
+  
 
 export default MeetupDetails
